26039bf7e10ae03bc8c847658711d9509b74132c
Kishore <35412658+useronezero@users.noreply.github.com>
2018-05-29 14:43:09 +0530

    creating menu

--- banking_application/src/banking_application/Banking_application.java
+++ banking_application/src/banking_application/Banking_application.java
@@ -1,11 +1,26 @@
 
 package banking_application;
 
+import java.util.Scanner;
+
 public class Banking_application {
 
    
     public static void main(String[] args) {
-   
+        int n=0;
+        Scanner s = new Scanner(System.in);
+        while(true) {
+            System.out.println("1.Add a customer\n"
+                              +"2.Deposit Money\n"
+                              +"3.Withdraw Money\n"
+                              +"4.Transfer Money\n"
+                              +"5.Total money in bank\n"
+                              +"6.Person with highest deposit\n"
+                              +"7.Exit");
+            System.out.print("Please Enter your choice : ");
+            int choice = s.nextInt();
+            
+        }
     }
     
 }
