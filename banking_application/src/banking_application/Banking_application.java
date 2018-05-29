c19e4b010a6e05ef28b5e1254ba2c41d241b1403
Kishore <35412658+useronezero@users.noreply.github.com>
2018-05-29 20:43:50 +0530

    Needs testing.

--- banking_application/src/banking_application/Banking_application.java
+++ banking_application/src/banking_application/Banking_application.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 class customer {
     Scanner s = new Scanner(System.in);
     private int accno;
-    private String name,address;
+    String name,address;
     double balance;
     customer(int i){
         accno=i;
@@ -55,6 +55,7 @@ class customer {
                 System.out.println("invalid withdrawl amount. Please try again");
         }
     }
+    
 }
 
 public class Banking_application {
@@ -80,8 +81,71 @@ public class Banking_application {
                             newcust.addcustomer();
                             i++;
                             cust.add(newcust);
+                            break;
                 }
+                case 2 : {
+                           System.out.print("Please enter your Account number : ");
+                           int input = s.nextInt();
+                           cust.get(input).deposit();
+                           break;
+                }
+                case 3 : {
+                           System.out.print("Please enter your Account number : ");
+                           int input = s.nextInt();
+                           cust.get(input).deposit();
+                           break;
+                }
+                case 4 : {
+                            System.out.print("Enter the your account number : ");
+                            int youracc = s.nextInt();
+                            System.out.print("Please enter the beneficiary Account number : ");
+                            int benacc=s.nextInt();
+                            if((i>youracc)&&(i>benacc)){
+                                double yourcurbal = cust.get(youracc).balance;
+                                System.out.print("Please enter the amount to transfer : ");
+                                double traamt = s.nextDouble();
+                                if((yourcurbal-traamt)>= 500) {
+                                    cust.get(youracc).balance -= traamt;
+                                    cust.get(benacc).balance += traamt;
+                                    break;
+                                }
+                                else {
+                                    System.out.println("Insufficent balance. Transfer Failed");
+                                    break;
+                                }
                             }
+                            else {
+                                System.out.print("invaild Account Numbers.Try again");
+                                break;
+                            }
+                }
+                case 5 : {
+                            double sum=0;
+                            for(int j=0;j<i;j++)
+                            {
+                                sum += cust.get(j).balance;
+                                System.out.println("The Total Money in bank is : "+sum);
+                                break;
+                            }
+                }
+                case 6 : { 
+                            int highacc=0;
+                            double highbal = cust.get(0).balance;
+                            for(int j=1;j<i;j++) {
+                                double bal = cust.get(j).balance;
+                                if(highbal<bal){
+                                    highbal = bal;
+                                    highacc = j;
+                                }
+                            }
+                            String name = cust.get(highacc).name;
+                            System.out.println("The person with highest bank balance is : "+name+"With balance : "+highbal);
+                            break;
+                }
+                default : {
+                            System.exit(0);
+                }
+                     }
         }
     }
     
