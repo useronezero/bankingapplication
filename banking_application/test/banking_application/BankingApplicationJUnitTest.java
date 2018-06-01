ba4adcad19400a4d91371f3c4f65b8303d0a41bc
Kishore <35412658+useronezero@users.noreply.github.com>
2018-06-02 00:35:22 +0530

    initial commit for Test unit.

--- banking_application/test/banking_application/BankingApplicationJUnitTest.java
+++ banking_application/test/banking_application/BankingApplicationJUnitTest.java
@@ -5,6 +5,8 @@
  */
 package banking_application;
 
+import java.util.ArrayList;
+import junit.framework.Assert;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -14,15 +16,20 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author kisho
+ * @author kishore
  */
 public class BankingApplicationJUnitTest {
+   static BankingApplication bank ;
+   static Customer c;
+   static ArrayList<Customer> customerArray ;
     
     public BankingApplicationJUnitTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+        bank = new BankingApplication();
+        c = new Customer(0,"default","default",1000);
     }
     
     @AfterClass
@@ -37,9 +44,44 @@ public class BankingApplicationJUnitTest {
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void CreateAccountTest() {
+        assertEquals(0,bank.addingCustomer(1,"Rahul","141 street",500.0));
+    }
+    @Test
+    public void CreateAccountTest1() {
+        assertEquals(0,bank.addingCustomer(1,"Sunil","222 street",5000.0));
+    }
+    @Test
+    public void DepositTest() {
+       assertEquals(0.0, c.deposit(1000), 0.001);
+    }
+    @Test
+    public void DepositTest1() {
+       assertEquals(0.0, c.deposit(500), 0.001);
+    }
+    @Test
+    public void WithdrawTest() {
+       assertEquals(0.0, c.withdraw(1000), 0.001);
+    }
+    @Test
+    public void WithdrawTest1() {
+       assertEquals(0.0, c.withdraw(500), 0.001);
+    }
+    @Test
+    public void TotalMoneyInBankTest() {
+       assertEquals(1,bank.totalMoneyInBank(),0.001);
+    }
+    @Test
+    public void TotalMoneyInBankTest1() {
+       assertEquals(0,bank.totalMoneyInBank(),0.001);
+    }
+    @Test
+    public void RichestPersonInBankTest() {
+       assertEquals(1,bank.richestPerson(),0.001);
+    }
+    @Test
+    public void RichestPersonInBankTest1() {
+       assertEquals(1,bank.richestPerson(),0.001);
+    }
 }
